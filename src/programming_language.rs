6f9e346523e5599d6ebbb59fcc56949a577e01fc
Heman Gandhi <hemangandhi@gmail.com>
2024-10-12 10:41:54 +0900

    Use the new literals predicate and fix formatting

--- src/programming_language.rs
+++ src/programming_language.rs
@@ -36,18 +36,18 @@ impl<'a> SExpr<'a> {
         let mut idx = 0;
         let mut inner_vec = vec![];
         while idx < input.len() {
-            if &input[idx..idx+1] == "(" {
-                let (sub, increment) = Self::parse_expr(&input[idx+1..])?;
+            if &input[idx..idx + 1] == "(" {
+                let (sub, increment) = Self::parse_expr(&input[idx + 1..])?;
                 inner_vec.push(SExpr::Expr(sub));
                 idx += increment;
-                if &input[idx..idx+1] != ")" {
+                if &input[idx..idx + 1] != ")" {
                     return Err(vec!["mismatchd parens".into()]);
                 }
                 continue;
             }
-            let literal_len = consume_chars(&input[idx..], |&c| c.is_alphanumeric());
+            let literal_len = consume_chars(&input[idx..], is_music_lang_literal);
             if literal_len > 0 {
-                inner_vec.push(SExpr::Literal(&input[idx..idx+literal_len]));
+                inner_vec.push(SExpr::Literal(&input[idx..idx + literal_len]));
                 idx += literal_len;
                 continue;
             }
@@ -59,15 +59,19 @@ impl<'a> SExpr<'a> {
     fn parse(input: &'a str) -> Result<Self, Vec<String>> {
         if input.len() < 1 || &input[0..1] != "(" {
             let non_space_start = consume_chars(&input, |&c| c.is_whitespace());
-            let token_len = consume_chars(&input[non_space_start..], |&c| c.is_alphanumeric());
-            let space_after_tok = consume_chars(&input[non_space_start + token_len..], |&c| c.is_whitespace());
+            let token_len = consume_chars(&input[non_space_start..], is_music_lang_literal);
+            let space_after_tok = consume_chars(&input[non_space_start + token_len..], |&c| {
+                c.is_whitespace()
+            });
             if non_space_start + token_len + space_after_tok != input.len() {
                 return Err(vec!["Invalid token!".into()]);
             }
-            return Ok(SExpr::Literal(&input[non_space_start..non_space_start + token_len]));
+            return Ok(SExpr::Literal(
+                &input[non_space_start..non_space_start + token_len],
+            ));
         }
         let (inner_expr, end) = Self::parse_expr(&input[1..])?;
-        if end + 1 == input.len() || &input[end+1..end+2] != ")" {
+        if end + 1 == input.len() || &input[end + 1..end + 2] != ")" {
             return Err(vec!["Missing ')'".into()]);
         }
         Ok(SExpr::Expr(inner_expr))
